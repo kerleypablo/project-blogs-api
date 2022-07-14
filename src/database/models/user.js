95631bc608990e7b890a7186ee754f317c5cde5d
kerley <kerley.kls@gmail.com>
2022-07-14 19:04:50 -0300

    feat: :sparkles: criado model User

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,23 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER, 
+      primarykey: true,
+      autoIncrement: true,
+    },
+    displayName: {
+      Type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    }});
+
+    User.associate = (models) => {
+      User.hasMany(models.BlogPosts, {
+        foreignKey: 'userId', as: 'blogPosts'})
     }
-  };
-  User.init({
-    displayName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+
   return User;
 };
+
+module.exports = User;
