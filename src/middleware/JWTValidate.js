e01fbb315133539dce50105cb76ea784cd2d7991
kerley <kerley.kls@gmail.com>
2022-07-15 18:42:32 -0300

    feat: :sparkles: criado validacao de token

---
+++ src/middleware/JWTValidate.js
@@ -0,0 +1,20 @@
+const JWT = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
+const validateJWT = (req, res, next) => {
+    const { token } = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token not found' });
+    }
+
+    try {
+      JWT.verify(token, secret);
+    } catch (error) {
+        return res.status(401).json({ message: error.message });
+    }
+    next();
+};
+
+module.exports = validateJWT;
