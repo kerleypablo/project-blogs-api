3a9f486ce5cdb3b466df8e95ef5e2618a7cda505
kerley <kerley.kls@gmail.com>
2022-07-18 15:13:40 -0300

    feat: criado model categorys

---
+++ src/database/models/category.js
@@ -0,0 +1,21 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  },
+    {
+      timestamps: false,
+      tableName: 'Categories',
+    },
+  );
+
+  return Category;
+};
+
+module.exports = Category;
