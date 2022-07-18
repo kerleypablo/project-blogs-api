9f211b2a347896f8c03dfedbd5f12b265bbe2511
kerley <kerley.kls@gmail.com>
2022-07-18 18:46:31 -0300

    feat: criado model postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,35 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+    },
+  },
+    {
+      timestamps: false,
+    },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
